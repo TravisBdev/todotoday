cd2c1918022b103e8d27db7907b31a9a80108bfa
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2022-07-22 13:21:02 -0400

    added the ability to append todos to the page. still working on deleting

--- app.js
+++ app.js
@@ -1,43 +1,61 @@
 const title = document.querySelector('#title');
 const mainWrapper = document.querySelector('#main-wrapper');
 const todoBtn = document.querySelector('#todo-btn');
-const confirmBtn = document.querySelector('.confirm-btn');
+const confirmBtn = document.querySelector('#confirm-btn');
 
+
+// initial button style on load
+confirmBtn.style.display = 'none';
+
+// event listeners
+window.addEventListener('dblclick', (e) => {
+  let item = document.querySelector('.item');
+  if(e.target === item){
+    item.parentElement.remove(item);
+  }
+})
 
 
 mainWrapper.addEventListener('click', (e) => {
   if(e.target === todoBtn) {
     buildTextAndButton();
-  } else if(e.target === confirmBtn) {
-     addTodo();
   }
+})
+
+confirmBtn.addEventListener('click', (e) => {
+  addTodo();
 })
 
 
 
 
 
+// helper functions
 function buildTextAndButton() {
   title.style.display = 'none';
+  confirmBtn.style.display = '';
   const input = document.createElement('input');
-  const confirmBtn = document.createElement('button');
-
   input.classList.add('input')
   mainWrapper.appendChild(input);
-
   todoBtn.style.display = 'none';
-  confirmBtn.classList.add('confirm-btn');
-  confirmBtn.textContent = 'Confirm';
-  mainWrapper.appendChild(confirmBtn);
-
+  // build list space for todos
+  let list = document.createElement('ul');
+  list.classList.add('list');
+  confirmBtn.insertAdjacentElement('afterend', list);
 }
 
 
 
 function addTodo(){
   const input = document.querySelector('.input');
-  let todoItem = document.createElement('p');
+  const ul = document.querySelector('.list');
   let todo = input.value;
-  todoItem.textContent = todo;
-  input.style.display = 'none';
+  let item = document.createElement('li')
+  ul.appendChild(item);
+  item.classList.add('item');
+  item.textContent = todo;
+  input.value = '';
 }
+
+
+
